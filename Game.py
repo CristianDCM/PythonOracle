ab5efb45ad2b0749d4c98f7abc1383a85f051487
CristianDC <cristianzdmc14@gmail.com>
2022-10-24 21:50:50 -0500

    Se optimizo y se quito varias mondades

--- Game.py
+++ Game.py
@@ -1,42 +1,42 @@
 import random
 from pyfiglet import Figlet # Librery = pip install pyfiglet==0.7
-Font = Figlet(font='slant') # Fuente del título
+Font = Figlet(font='slant') 
 
-def main(buscar_comida, tablero, culebrita, comida, mover_culebrita): # Función principal
+def main(buscar_comida, tablero, culebrita, comida, mover_culebrita): 
     print("="*50)
-    print(Font.renderText('Culebrita')) # Imprimir el título
+    print(Font.renderText('Culebrita')) 
     print("="*50)
     tablero = tablero()
     comida(tablero)
     culebrita = culebrita()
-    lista_culebrita = [] # Lista para guardar la posición de la culebrita
-    longitud_culebrita = 1 # Longitud de la culebrita
+    lista_culebrita = [] 
+    longitud_culebrita = 1
     while True:
-        mover_culebrita(culebrita, comida, tablero) # Llamar a la función mover_culebrita
+        mover_culebrita(culebrita, comida, tablero) 
         
-        cabeza_culebrita = [posicion_actual[0], posicion_actual[1]] # Posición de la cabeza de la culebrita
-        lista_culebrita.append(cabeza_culebrita) # Agregar la posición de la cabeza de la culebrita a la lista
+        cabeza_culebrita = [posicion_actual[0], posicion_actual[1]]
+        lista_culebrita.append(cabeza_culebrita) 
 
-        if len(lista_culebrita) > longitud_culebrita: # Si la longitud de la lista es mayor a la longitud de la culebrita
-            del lista_culebrita[0] # Eliminar el primer elemento de la lista
+        if len(lista_culebrita) > longitud_culebrita:
+            del lista_culebrita[0] 
 
-        for i in range(len(lista_culebrita)): # Recorrer la lista
-            tablero[lista_culebrita[i][0]][lista_culebrita[i][1]] = culebrita # Agregar la posición de la culebrita en el tablero
+        for i in range(len(lista_culebrita)): 
+            tablero[lista_culebrita[i][0]][lista_culebrita[i][1]] = culebrita 
 
-        if buscar_comida(posicion_actual, posicion_comida) == True: # Si la posición actual es igual a la posición de la comida
-            comida(tablero) # Agregar comida
-            longitud_culebrita += 1    # Aumentar la longitud de la culebrita
-            print(lista_culebrita) # Imprimir la lista
-            print(longitud_culebrita) # Imprimir la longitud de la culebrita
+        if buscar_comida(posicion_actual, posicion_comida) == True: 
+            comida(tablero) 
+            longitud_culebrita += 1 
+            print(lista_culebrita) 
+            print(longitud_culebrita) 
         
 
-        for i in range(10): # Recorrer el tablero
-            print(" ".join(tablero[i])) # Imprimir el tablero
-        print("="*50) # Separador
+        for i in range(10): 
+            print(" ".join(tablero[i]))
+        print("="*50) 
         print("")
 
 
-def buscar_comida(posicion_actual, posicion_comida): # Función para buscar la comida
+def buscar_comida(posicion_actual, posicion_comida): 
     if posicion_actual == posicion_comida:
         return True
 
@@ -47,32 +47,32 @@ def culebrita():
 def comida(tablero):
     global posicion_comida 
     comida = ("✩")
-    posicion_comida = [random.randint(0, 9), random.randint(0, 9)] # Posición de la comida
-    tablero[posicion_comida[0]][posicion_comida[1]] = comida # Agregar la comida en el tablero
+    posicion_comida = [random.randint(0, 9), random.randint(0, 9)] 
+    tablero[posicion_comida[0]][posicion_comida[1]] = comida 
 
 
 def tablero():
-    tablero = [] # Lista para guardar el tablero
-    for i in range(10): # Recorrer el tablero
-        tablero.append(["O"]*10) # Agregar "O" en el tablero
+    tablero = [] 
+    for i in range(10):
+        tablero.append(["O"]*10) 
     return tablero
 
 
 def mover_culebrita(culebrita, comida, tablero):
-    global posicion_actual  # Variable global
+    global posicion_actual 
     mover_culebrita = input("¿Dónde quieres mover la culebrita? (W, A, S, D): ")
-    posicion_actual = [0, 0] # Posición actual de la culebrita
-    for i in range(10): # Recorrer el tablero en el eje Y
-        for j in range(10): # Recorrer el tablero en el eje X
-            if tablero[i][j] == culebrita: # Si el tablero en la posición i, j es igual a la culebrita
-                posicion_actual = [i, j] # Posición actual de la culebrita
+    posicion_actual = [0, 0] 
+    for i in range(10): 
+        for j in range(10): 
+            if tablero[i][j] == culebrita: 
+                posicion_actual = [i, j] 
 
-    if mover_culebrita == "w" or mover_culebrita == "W": # Si la tecla presionada es W
-        if posicion_actual[0] == 0: # Si la posición actual en el eje Y es igual a 0
-            print("No puedes salirte del tablero") # Imprimir mensaje
-        else: # Si no
-            tablero[posicion_actual[0]][posicion_actual[1]] = "O" # Agregar "O" en la posición actual de la culebrita
-            tablero[posicion_actual[0]-1][posicion_actual[1]] = culebrita # Agregar la culebrita en la posición actual - 1
+    if mover_culebrita == "w" or mover_culebrita == "W": 
+        if posicion_actual[0] == 0: 
+            print("No puedes salirte del tablero") 
+        else: 
+            tablero[posicion_actual[0]][posicion_actual[1]] = "O" 
+            tablero[posicion_actual[0]-1][posicion_actual[1]] = culebrita 
 
     elif mover_culebrita == "a" or mover_culebrita == "A":
         if posicion_actual[1] == 0:
@@ -97,5 +97,5 @@ def mover_culebrita(culebrita, comida, tablero):
 
 
 
-if __name__ == "__main__": # Si el archivo se ejecuta directamente
-    main(buscar_comida, tablero, culebrita, comida, mover_culebrita) # Ejecutar la función main
+if __name__ == "__main__": 
+    main(buscar_comida, tablero, culebrita, comida, mover_culebrita)
