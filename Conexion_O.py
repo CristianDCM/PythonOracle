cf3e13c114c87f37bb313e4cee7428b326dd2508
Cristian David <98545319+CristianDCM@users.noreply.github.com>
2022-10-25 22:14:56 -0500

    V1

--- Conexion_O.py
+++ Conexion_O.py
@@ -1,13 +1,29 @@
-
 import cx_Oracle
-try:
-    con = cx_Oracle.connect('system/12345@localhost:1521/xe')
-    print("Conexion exitosa")
-except cx_Oracle.DatabaseError as e:
-    print("Error al conectar", e)
+import tkinter as tk
 
-cursor = con.cursor()
-cursor.execute("select * from ESTUDIANTES")
-for row in cursor:
-    print(row)
+def Conexion_O():
+    try:
+        con = cx_Oracle.connect('system/12345@localhost:1521/xe')
+        print("Conexion exitosa")
+    except cx_Oracle.DatabaseError as e:
+        print("Error al conectar", e)
+    return con
 
+def Desconectar_O(con):
+    try:
+        con.close()
+        print("Desconexion exitosa")
+    except cx_Oracle.DatabaseError as e:
+        print("Error al desconectar", e)
+
+#crear ventana principal
+ventana = tk.Tk()
+ventana.title("Conexion Oracle")
+ventana.geometry("400x400")
+
+boton = tk.Button(ventana, text="Conectar", command=Conexion_O)
+boton.pack()
+#boton desconectar
+boton2 = tk.Button(ventana, text="Desconectar", command=Desconectar_O(Conexion_O()))
+boton2.pack()
+ventana.mainloop()
