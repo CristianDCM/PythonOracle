a78cc557d024879ab71005aa3515a73310aadd2d
Cristian David <98545319+CristianDCM@users.noreply.github.com>
2022-10-25 21:41:58 -0500

    Delete Conexion.py

--- Python_Oracle/Conexion.py
+++
@@ -1,9 +0,0 @@
-import cx_Oracle
-#formato de conexion
-con = cx_Oracle.connect("usuario/contraseña@ip:puerto/sid")
-#ejecutar consulta
-cursor = con.cursor()
-cursor.execute("select * from tabla")
-#recorrer consulta
-for row in cursor:
-    print(row)
